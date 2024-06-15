91d8c34bec594ee4739c159396a16280c58e4cd2
Steve <falecomstevevaz@gmail.com>
2024-06-15 12:42:33 -0300

    Commit the scripts

---
+++ src/demo_dao_jdbc/db/DBException.java
@@ -0,0 +1,11 @@
+package demo_dao_jdbc.db;
+
+public class DBException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public DBException(String msg) {
+		super(msg);
+	}
+
+}
